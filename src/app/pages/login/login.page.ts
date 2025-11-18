39b3e659ca3c08f175be148192610002e6ee246a
Squawks1 <to.toledom@duocuc.cl>
2025-11-17 23:30:05 -0300

    update login.page
    se actualiza contenido de login page, asi tambien como las funciones

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DbtaskService } from 'src/app/services/dbtask';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
   standalone: false
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  user = '';
+  pass: any = '';
 
-  ngOnInit() {
+  constructor(private db: DbtaskService, private router: Router) {}
+
+  async login() {
+    const result = await this.db.validarLogin(this.user, this.pass);
+
+    if (result.rows.length > 0) {
+      await this.db.actualizarEstado(this.user, 1);
+      await this.db.guardarStorage(this.user);
+      alert('Inicio exitoso');
+      this.router.navigate(['/home']);
+    } else {
+      alert('Usuario o clave incorrectos');
+    }
   }
 
+  async registrar() {
+    try {
+      await this.db.registrarUsuario(this.user, this.pass);
+      await this.db.guardarStorage(this.user);
+      alert('Registrado correctamente');
+      this.router.navigate(['/home']);
+    } catch {
+      alert('Usuario ya existe');
+    }
+  }
 }
