8a5f8cbd5978151a42182920a52ee4196754cda8
Squawks1 <to.toledom@duocuc.cl>
2025-11-24 20:03:52 -0300

    Update api-rest.ts
    se configuran los métodos de la api

--- src/app/services/api-rest.ts
+++ src/app/services/api-rest.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class ApiRest {
-  
+export class ApiRestService {
+
+  apiURL = 'http://localhost:3000';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) {}
+
+  // Ver usuario (GET)
+  getUsers(): Observable<any> {
+    return this.http.get(this.apiURL + '/usuarios');
+  }
+
+  // Crear usuario (POST)
+  createUser(data: any): Observable<any> {
+    return this.http.post(this.apiURL + '/usuarios', data, this.httpOptions);
+  }
+
+  // Eliminar (DELETE)
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete(this.apiURL + '/usuarios' + id, this.httpOptions);
+  }
 }
