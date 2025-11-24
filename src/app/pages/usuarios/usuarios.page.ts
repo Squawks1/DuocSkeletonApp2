6b04053c1fc53200ceb3e5828f1d0f81548f597f
Squawks1 <to.toledom@duocuc.cl>
2025-11-24 20:45:38 -0300

    Update usuarios.page.ts
    se agrega configuración de la api

--- src/app/pages/usuarios/usuarios.page.ts
+++ src/app/pages/usuarios/usuarios.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiRestService } from '../../services/api-rest';
 
 @Component({
   selector: 'app-usuarios',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsuariosPage implements OnInit {
 
-  constructor() { }
+  users: any[] = [];
+
+  constructor(private api: ApiRestService) {}
 
   ngOnInit() {
+    this.loadUsers();
   }
 
+  // Método GET
+
+  loadUsers() {
+    this.api.getUsers().subscribe((res: any) => {
+      this.users = res;
+    });
+  }
+
+  // Método POST
+
+  addUser() {
+    const nuevo = {
+      nombre: "Usuario Nuevo",
+      email: "nuevo@example.com"
+    };
+
+    this.api.createUser(nuevo).subscribe(() => {
+      this.loadUsers();
+    });
+  }
+
+  // Método DELETE
+  
+  deleteUser(id: number) {
+    this.api.deleteUser(id).subscribe(() => {
+      this.loadUsers();
+    });
+  }
 }
