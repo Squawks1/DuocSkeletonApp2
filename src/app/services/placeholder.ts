024e16d1c0559676186359fdc399113782d11cac
Squawks1 <to.toledom@duocuc.cl>
2025-11-24 20:47:21 -0300

    Update placeholder.ts
    configuracion placeholder.ts

--- src/app/services/placeholder.ts
+++ src/app/services/placeholder.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Placeholder {
-  
+export class PlaceholderService {
+
+  baseURL = 'https://jsonplaceholder.typicode.com';
+
+  constructor(private http: HttpClient) {}
+
+  getPosts() {
+    return this.http.get(this.baseURL + '/posts');
+  }
+
+  createPost(data: any) {
+    return this.http.post(this.baseURL + '/posts', data);
+  }
 }
