03b25377dad625d0b650671b3db36d182d32b767
Squawks1 <to.toledom@duocuc.cl>
2025-11-24 21:24:00 -0300

    Update app-routing.module.ts
    modificado error de modulos ***, colocandolo al final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { AuthGuard } from './guards/auth-guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule), canActivate: [AuthGuard] },
-  { path: '**', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundPageModule) },
+  { path: 'login', 
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) 
+  },
+  { path: 'home', 
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule), canActivate: [AuthGuard] 
+  },
   {
     path: 'usuarios',
     loadChildren: () => import('./pages/usuarios/usuarios.module').then( m => m.UsuariosPageModule)
@@ -18,7 +21,10 @@ const routes: Routes = [
   {
     path: 'camara',
     loadChildren: () => import('./pages/camara/camara.module').then( m => m.CamaraPageModule)
-  }
+  },
+  { path: '**', 
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundPageModule) 
+  },
 ];
 
 @NgModule({
