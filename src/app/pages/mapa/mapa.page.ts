631b0af0b624346c751e7cf04559808903d29690
Squawks1 <to.toledom@duocuc.cl>
2025-11-24 21:19:18 -0300

    Update mapa.page
    se actualiza la page del mapa dado que se intenta vincular con api Google Maps creada de GoogleCloud

--- src/app/pages/mapa/mapa.page.ts
+++ src/app/pages/mapa/mapa.page.ts
@@ -1,19 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
+
+declare var google: any;
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.page.html',
   styleUrls: ['./mapa.page.scss'],
 })
-export class MapaPage {
+export class MapaPage implements AfterViewInit {
 
-  lat: number = 0;
-  lng: number = 0;
+  map: any;
 
-  async obtenerUbicacion() {
-    const pos = await Geolocation.getCurrentPosition();
-    this.lat = pos.coords.latitude;
-    this.lng = pos.coords.longitude;
+  async ngAfterViewInit() {
+    await this.loadGoogleMaps();
+    await this.showMap();
+  }
+
+  loadGoogleMaps(): Promise<void> {
+    return new Promise((resolve) => {
+      // Si el script ya está cargado, no lo cargues otra vez
+      if (document.getElementById('googleMapsScript')) {
+        resolve();
+        return;
+      }
+
+      const script = document.createElement('script');
+      script.id = 'googleMapsScript';
+      script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAyzFTinCKELqPvAAjgkZebcIhamrLcjgo';
+      script.async = true;
+      script.defer = true;
+      script.onload = () => resolve();
+
+      document.body.appendChild(script);
+    });
+  }
+
+  async showMap() {
+    const position = await Geolocation.getCurrentPosition();
+
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+
+    const mapElement = document.getElementById('map');
+
+    this.map = new google.maps.Map(mapElement, {
+      center: { lat, lng },
+      zoom: 16
+    });
+
+    new google.maps.Marker({
+      position: { lat, lng },
+      map: this.map,
+      title: 'Mi posición actual'
+    });
   }
 }
