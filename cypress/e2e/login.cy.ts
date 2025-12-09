83a3c9c7d076b27090245dadc3481cca17b8f181
Squawks1 <to.toledom@duocuc.cl>
2025-12-08 22:44:47 -0300

    Create login.cy.ts
    Se crea archivo para pruebas en cypress

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,19 @@
+describe('Prueba E2E', () => {
+
+    it('Debería mostrar el título correcto', () => {
+        cy.visit('/');
+        cy.contains('ion-title', 'Iniciar Sesión').should('be.visible');
+    });
+
+    it('Debería permitir ingresar al Home con credenciales válidas', () => {
+        cy.visit('/');
+
+        cy.get('#user').type('admin');
+        cy.get('#pass').type('1234');
+
+        cy.get('#btnLogin').click();
+
+        cy.url().should('include', '/home');
+    });
+
+});
