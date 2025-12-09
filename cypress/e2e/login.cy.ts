b07b886451f89a85e2131ad7a9479c3e44bb38ed
Squawks1 <to.toledom@duocuc.cl>
2025-12-08 23:40:11 -0300

    Update login.cy.ts
    intentando solucionar errores

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,17 +1,17 @@
 describe('Prueba E2E', () => {
 
     it('Debería mostrar el título correcto', () => {
-        cy.visit('/');
+        cy.visit('/login');
         cy.contains('ion-title', 'Iniciar Sesión').should('be.visible');
     });
 
     it('Debería permitir ingresar al Home con credenciales válidas', () => {
         cy.visit('/login');
 
-        cy.get('#user').type('admin');
-        cy.get('#pass').type('1234');
+        cy.get('user').type('admin');
+        cy.get('pass').type('1234');
 
-        cy.get('#btnLogin').click();
+        cy.get('btnLogin').click();
 
         cy.url().should('include', '/home');
     });
